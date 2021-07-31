f8cc34ab354d873de765e2542199a4baf4771209
Misat11 <misa.turek1@gmail.com>
2021-07-31 18:45:37 +0200

    fix when bot leaves the guild and there are registered tickets channel in that guild

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -19,33 +19,37 @@ class ReadyListener extends Listener {
             let channels = ChannelConfigurationManager.getAllChannels();
 
             for (let channelConfiguration of channels) {
-                let guild = (await event.guilds.fetch(channelConfiguration.guildId));
-                let channel = guild.channels.resolve(channelConfiguration.channelId);
-                if (channel.isText) {
-                    channel.messages.fetch().then(value => {
-                        if (value.find(msg => msg.author.id === client.id && msg.embeds.length > 0 && msg.embeds[0].footer.text.includes("ticketWelcome"))) {
-                            channel.send({
-                                embeds: [
-                                    new MessageEmbed({
-                                        color: "AQUA",
-                                        title: channelConfiguration.title || Messages.DEFAULT_TICKET_WELCOME_TITLE,
-                                        description: (channelConfiguration.description || Messages.DEFAULT_TICKET_WELCOME_DESCRIPTION).replace('${channel}', '<#' + channelConfiguration.channelId + '>'),
-                                        footer: {
-                                            text: 'Screamer v' + version + ' ticketWelcome'
-                                        }
-                                    })
-                                ],
-                                components: [
-                                    new MessageActionRow({
-                                        components: [
-                                            CreateTicketInteractionHandler.button,
-                                            MyTicketsInteractionHandler.button
-                                        ]
-                                    })
-                                ]
-                            });
-                        }
-                    });
+                try {
+                    let guild = (await event.guilds.fetch(channelConfiguration.guildId));
+                    let channel = guild.channels.resolve(channelConfiguration.channelId);
+                    if (channel.isText) {
+                        channel.messages.fetch().then(value => {
+                            if (value.find(msg => msg.author.id === client.id && msg.embeds.length > 0 && msg.embeds[0].footer.text.includes("ticketWelcome"))) {
+                                channel.send({
+                                    embeds: [
+                                        new MessageEmbed({
+                                            color: "AQUA",
+                                            title: channelConfiguration.title || Messages.DEFAULT_TICKET_WELCOME_TITLE,
+                                            description: (channelConfiguration.description || Messages.DEFAULT_TICKET_WELCOME_DESCRIPTION).replace('${channel}', '<#' + channelConfiguration.channelId + '>'),
+                                            footer: {
+                                                text: 'Screamer v' + version + ' ticketWelcome'
+                                            }
+                                        })
+                                    ],
+                                    components: [
+                                        new MessageActionRow({
+                                            components: [
+                                                CreateTicketInteractionHandler.button,
+                                                MyTicketsInteractionHandler.button
+                                            ]
+                                        })
+                                    ]
+                                });
+                            }
+                        });
+                    }
+                } catch (e) {
+                    console.error(e);
                 }
             }
         });
